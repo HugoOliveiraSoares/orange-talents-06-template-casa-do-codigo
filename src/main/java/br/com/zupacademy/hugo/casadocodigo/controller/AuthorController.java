80339df4711ad601eb47cf88ab2ffec03be6e349
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-06-29 17:11:32 -0300

    Email autor único

--- src/main/java/br/com/zupacademy/hugo/casadocodigo/controller/AuthorController.java
+++ src/main/java/br/com/zupacademy/hugo/casadocodigo/controller/AuthorController.java
@@ -4,10 +4,12 @@ import br.com.zupacademy.hugo.casadocodigo.controller.dto.AuthorDTO;
 import br.com.zupacademy.hugo.casadocodigo.controller.form.AuthorFORM;
 import br.com.zupacademy.hugo.casadocodigo.model.Author;
 import br.com.zupacademy.hugo.casadocodigo.repository.AuthorRepository;
+import br.com.zupacademy.hugo.casadocodigo.vallidator.UniqueEmailAuthor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import javax.validation.Valid;
@@ -20,6 +22,14 @@ public class AuthorController {
     @Autowired
     private AuthorRepository authorRepository;
 
+    @Autowired
+    private UniqueEmailAuthor uniqueEmailAuthor;
+
+    @InitBinder
+    public void init(WebDataBinder binder){
+        binder.addValidators(uniqueEmailAuthor);
+    }
+
     @GetMapping
     public Page<AuthorDTO> list(Pageable pages){
 
@@ -31,12 +41,20 @@ public class AuthorController {
     @PostMapping
     public ResponseEntity<AuthorDTO> register(@RequestBody @Valid AuthorFORM authorFORM, UriComponentsBuilder uriComponentsBuilder){
 
+//        if (authorRepository.findByEmail( authorFORM.getEmail() ) == null){
+//            Author author = authorFORM.convert(authorFORM);
+//            authorRepository.save(author);
+//
+//            URI uri = uriComponentsBuilder.path("author/{id}").buildAndExpand(author.getId()).toUri();
+//            return ResponseEntity.created(uri).body(new AuthorDTO(author));
+//        }
+//        return ResponseEntity.badRequest().body("Email Duplicado");
+
         Author author = authorFORM.convert(authorFORM);
         authorRepository.save(author);
 
         URI uri = uriComponentsBuilder.path("author/{id}").buildAndExpand(author.getId()).toUri();
         return ResponseEntity.created(uri).body(new AuthorDTO(author));
-
     }
 
 }
--- src/main/java/br/com/zupacademy/hugo/casadocodigo/repository/AuthorRepository.java
+++ src/main/java/br/com/zupacademy/hugo/casadocodigo/repository/AuthorRepository.java
@@ -3,6 +3,9 @@ package br.com.zupacademy.hugo.casadocodigo.repository;
 import br.com.zupacademy.hugo.casadocodigo.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
+    Optional<Author> findByEmail(String email);
 }
